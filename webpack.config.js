6259a90642d7e3479084253188bb47fa537b1f0a
Anand <anandcoder1@gmail.com>
2025-08-04 07:20:42 +0530

    old expo removed

--- webpack.config.js
+++
@@ -1,6 +0,0 @@
-const createExpoWebpackConfigAsync = require("@expo/webpack-config")
-
-module.exports = async (env, argv) => {
-  const config = await createExpoWebpackConfigAsync(env, argv)
-  return config
-}
